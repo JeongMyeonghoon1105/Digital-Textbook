f238efa76f69a639c0b4ff3b504b7136b042a80e
JeongMyeonghoon1105 <wjdaudgns1105@naver.com>
2023-05-09 15:34:14 +0900

    Modal Added

--- scripts.js
+++ scripts.js
@@ -10,14 +10,42 @@ function openModal1() {
     modal1.style.display = 'block';
   }
 }
+
+function openModal2() {
+  var modal3 = document.getElementById("modal3");
+  var modal4 = document.getElementById("modal4");
+  var ans4 = document.querySelector("#answer4").value;
+  var ans5 = document.querySelector("#answer5").value;
+  var ans6 = document.querySelector("#answer6").value;
+  var ans7 = document.querySelector("#answer7").value;
+  var ans8 = document.querySelector("#answer8").value;
+  var ans9 = document.querySelector("#answer9").value;
+  var ans10 = document.querySelector("#answer10").value;
+  var ans11 = document.querySelector("#answer11").value;
+  if (ans4 == "" && ans5 == "" && ans6 == "" && ans7 == "" && ans8 == "" && ans9 == "" && ans10 == "" && ans11 == "") {
+    modal4.style.display = 'block';
+  } else {
+    modal3.style.display = 'block';
+  }
+}
+
 document.onclick = (event) => {
   var modal1 = document.getElementById("modal1");
   var modal2 = document.getElementById("modal2");
-  var answer = document.getElementById("answer");
-  if (event.target != modal1 && event.target != answer) {
+  var modal3 = document.getElementById("modal3");
+  var modal4 = document.getElementById("modal4");
+  var answer1 = document.getElementById("first-answer");
+  var answer2 = document.getElementById("learning-activity-answer-button");
+  if (event.target != modal1 && event.target != answer1) {
     modal1.style.display = "none";
   }
-  if (event.target != modal2 && event.target != answer) {
+  if (event.target != modal2 && event.target != answer1) {
     modal2.style.display = "none";
+  }
+  if (event.target != modal3 && event.target != answer2) {
+    modal3.style.display = "none";
+  }
+  if (event.target != modal4 && event.target != answer2) {
+    modal4.style.display = "none";
   }
 }
